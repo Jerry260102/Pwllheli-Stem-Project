5a23d6cd997e8baf09b3ed3c39b809ab01fd0f08
Jerry <jerzy.niechcial.2601@gmail.com>
2022-11-21 11:57:33 +0000

    Read CSV File and output amount of lines

--- PwllStem/src/pwllstem/PwllStem.java
+++ PwllStem/src/pwllstem/PwllStem.java
@@ -4,19 +4,39 @@
  */
 package pwllstem;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author jerzy
  */
-public class PwllStem {
 
+public class PwllStem {
+    private static final String filename = "csv/cog_metadata_0.0005_424736.csv";
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         // TODO code application logic here
-        System.out.println("Alright Cunt?");
-        
+        List<String> data = new ArrayList<>();
+        try {
+            FileInputStream fstream = new FileInputStream(filename);
+            DataInputStream in = new DataInputStream(fstream);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String line;
+            while ((line = br.readLine()) != null) {
+                data.add(line);
+            }
+        }catch (Exception ex){
+            throw new Exception("Error Opening File");
+        }
+        System.out.println(data.size());
     }
     
 }
