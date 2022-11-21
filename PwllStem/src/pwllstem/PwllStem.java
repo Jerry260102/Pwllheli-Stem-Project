ddd1a5d7e241525eadeeb52f4fdd7fcb2dee2706
Jerry <jerzy.niechcial.2601@gmail.com>
2022-11-21 12:18:01 +0000

    More Outputs
    Change outputs to;
    Total Tests in CSV
    Tests in England
    Tests in Wales
    Tests in Scotland
    Tests in Northern Ireland

--- PwllStem/src/pwllstem/PwllStem.java
+++ PwllStem/src/pwllstem/PwllStem.java
@@ -24,19 +24,36 @@ public class PwllStem {
      */
     public static void main(String[] args) throws Exception{
         // TODO code application logic here
-        List<String> data = new ArrayList<>();
+        List<String> wholeData = new ArrayList<>();
+        List<String> EnglandData = new ArrayList<>();
+        List<String> WalesData = new ArrayList<>();
+        List<String> ScotlandData = new ArrayList<>();
+        List<String> NIrelandData = new ArrayList<>();
         try {
             FileInputStream fstream = new FileInputStream(filename);
             DataInputStream in = new DataInputStream(fstream);
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             String line;
             while ((line = br.readLine()) != null) {
-                data.add(line);
+                if(line.contains("England")){
+                    EnglandData.add(line);
+                } else if (line.contains("Wales")) {
+                    WalesData.add(line);                    
+                } else if (line.contains("Scotland")) {
+                    ScotlandData.add(line);
+                } else if (line.contains("Northern_Ireland")) {
+                    NIrelandData.add(line);
+                }
+                wholeData.add(line);
             }
         }catch (Exception ex){
             throw new Exception("Error Opening File");
         }
-        System.out.println(data.size());
+        System.out.println("Amount of Tests in CSV: "+(wholeData.size()-1));
+        System.out.println("Amount of Tests in England: "+(EnglandData.size()));
+        System.out.println("Amount of Tests in Wales: "+(WalesData.size()));
+        System.out.println("Amount of Tests in Scotland: "+(ScotlandData.size()));
+        System.out.println("Amount of Tests in Northern Ireland: "+(NIrelandData.size()));
     }
     
 }
