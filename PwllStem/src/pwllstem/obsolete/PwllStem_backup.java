f8c76853fb5b3452eba6218be6514c4504153da2
Jerry <jerzy.niechcial.2601@gmail.com>
2022-12-09 13:43:56 +0000

    Eclipse Project Sync

--- PwllStem/src/pwllstem/obsolete/PwllStem_backup.java
+++ PwllStem/src/pwllstem/obsolete/PwllStem_backup.java
@@ -1,8 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package pwllstem;
+package pwllstem.obsolete;
 
 import java.io.BufferedReader;
 import java.io.DataInputStream;
